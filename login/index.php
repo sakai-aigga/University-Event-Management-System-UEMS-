506f1df3045cd8175e1b3cb4761ef9f468d270d2
sakai-aigga <sajinghimire2010@gmail.com>
2025-12-19 10:54:45 +0545

    Updates API and Assets

--- login/index.php
+++ login/index.php
@@ -1,5 +1,20 @@
 <?php
-include "db.php";
-
-echo "Database connected and index page loaded";
+include "../includes/db-config.php";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Form</title>
+</head>
+
+<body>
+    <form method="post" action="">
+        <input type="text" name="username" placeholder="Username" required>
+        <br><br>
+        <input type="text" name="password" placeholder="Password" required>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
