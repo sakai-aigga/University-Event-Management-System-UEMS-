1c6d6a595abbd3e63c8afee424719995a3dcff6c
sakai-aigga <sajin_btechedit2023@kusoed.edu.np>
2026-03-08 08:18:57 +0545

    Google app config for PHP mailer

---
+++ includes/mail-config.php
@@ -0,0 +1,9 @@
+<?php
+// SMTP Configuration
+define('MAILHOST', 'smtp.gmail.com');
+define('USERNAME', 'sajinghimire2010@gmail.com');
+define('PASSWORD', 'gnpk pqpm cias zjkd'); // Gmail App Password
+define('SEND_FROM', 'sajinghimire2010@gmail.com');
+define('SEND_FROM_NAME', 'UEMS Contact Form');
+define('RECEIVER_EMAIL', 'sajinghimire2010@gmail.com'); // Where to receive contact messages
+?>
